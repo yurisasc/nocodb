97ab3cfab18e4561fa42053e2b97517b4b678c03
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-06-30 19:09:11 +0800

    init: workflow dynamic page

---
+++ packages/nc-gui/pages/workflows/_id.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container fluid class="pa-0 ma-0" style="overflow: auto">
+    
+  </v-container>
+</template>
+<script>
+
+export default {
+  components: { },
+  data() {
+    return { }
+  },
+  computed: {
+    workflowId() {
+      return this.$route.params.id
+    },
+  },
+  async created() { },
+  mounted() { },
+  methods: { }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+<!--
+/**
+ * @copyright Copyright (c) 2021, Xgene Cloud Ltd
+ *
+ * @author Wing-Kam Wong <wingkwong.code@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+-->
