a54f0bf95693bf16d7008dc267220c7d1c941af2
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-04 13:07:24 +0800

    feat(nc-gui: directory tree

---
+++ packages/nc-gui/components/dashboard/storage-manager/DirectoryTree.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useStorageManagerStoreOrThrow } from '#imports'
+const { directoryTree, directoryTreeSelectedKeys, directoryTreeExpandedKeys } = useStorageManagerStoreOrThrow()
+</script>
+
+<template>
+  <a-tree
+    v-model:expandedKeys="directoryTreeExpandedKeys"
+    v-model:selectedKeys="directoryTreeSelectedKeys"
+    :tree-data="directoryTree"
+  >
+    <template #title="{ title }">
+      <span>{{ title }}</span>
+    </template>
+  </a-tree>
+</template>
