4cc1c7e08b13ffb7f2f1bb8ee539f4569ed41ea7
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-04 12:46:57 +0800

    feat(nc-gui): storage manager modal

---
+++ packages/nc-gui/components/dashboard/storage-manager/Modal.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { useI18n, useNuxtApp, useProvideStorageManagerStore, useUIPermission, useVModel } from '#imports'
+
+interface Props {
+  modelValue: boolean
+  openKey?: string
+}
+
+const props = defineProps<Props>()
+
+const emits = defineEmits(['update:modelValue'])
+
+const vModel = useVModel(props, 'modelValue', emits)
+
+const { isUIAllowed } = useUIPermission()
+
+const { t } = useI18n()
+
+const { $e } = useNuxtApp()
+
+useProvideStorageManagerStore()
+</script>
+
+<template>
+  <a-modal
+    v-model:visible="vModel"
+    :footer="null"
+    width="max(90vw, 600px)"
+    :closable="false"
+    wrap-class-name="nc-modal-settings"
+    @cancel="emits('update:modelValue', false)"
+  >
+    <!-- Storage Manager -->
+    <div class="flex flex-row justify-between w-full items-center mb-1">
+      <a-typography-title class="ml-4 select-none" type="secondary" :level="5">
+        {{ $t('title.storageManager') }}
+      </a-typography-title>
+
+      <a-button type="text" class="!rounded-md border-none -mt-1.5 -mr-1" @click="vModel = false">
+        <template #icon>
+          <MdiClose class="cursor-pointer mt-1 nc-modal-close" />
+        </template>
+      </a-button>
+    </div>
+
+    <a-layout class="mt-3 h-[75vh] overflow-y-auto flex">
+      <!-- Directory Tree -->
+      <a-layout-sider>
+        <LazyDashboardStorageManagerDirectoryTree />
+      </a-layout-sider>
+
+      <!-- Explorer -->
+      <a-layout-content class="h-auto px-4 scrollbar-thumb-gray-500">
+        <LazyDashboardStorageManagerExplorer />
+      </a-layout-content>
+    </a-layout>
+  </a-modal>
+</template>
