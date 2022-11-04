b41e8ed9434832d67b8b945b2e42fc40f23bdd10
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-04 14:18:22 +0800

    feat(nc-gui): storage manager breadcrumb

---
+++ packages/nc-gui/components/dashboard/storage-manager/Breadcrumb.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useStorageManagerStoreOrThrow } from '#imports'
+const { breadcrumbItems } = useStorageManagerStoreOrThrow()
+</script>
+
+<template>
+  <a-breadcrumb>
+    <!-- TODO: make each item clickable to navigate -->
+    <a-breadcrumb-item v-for="(breadcrumbItem, idx) of breadcrumbItems" :key="idx">{{ breadcrumbItem }}</a-breadcrumb-item>
+  </a-breadcrumb>
+</template>
