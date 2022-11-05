63d0073e60665005209a5b2cca3fb4f1b01a63ad
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-05 20:01:52 +0800

    wip(nc-gui): storage manager explorer

---
+++ packages/nc-gui/components/dashboard/storage-manager/Explorer.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useStorageManagerStoreOrThrow } from '#imports'
+const { breadcrumbItems } = useStorageManagerStoreOrThrow()
+</script>
+
+<template>
+  <LazyDashboardStorageManagerBreadcrumb />
+  <h1 class="prose-2xl font-bold mt-2">
+    {{ breadcrumbItems?.at(-1) }}
+  </h1>
+</template>
