a42f1beb19a67022700b851b754bfea7097de6d0
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-05 13:37:38 +0800

    feat(nocodb): add storage model

---
+++ packages/nocodb/src/lib/models/Storage.ts
@@ -0,0 +1,107 @@
+import Noco from '../Noco';
+import {
+  CacheDelDirection,
+  CacheGetType,
+  CacheScope,
+  MetaTable,
+} from '../utils/globals';
+import NocoCache from '../cache/NocoCache';
+import { StorageType } from 'nocodb-sdk';
+import { extractProps } from '../meta/helpers/extractProps';
+
+export default class Storage implements StorageType {
+  constructor(data: Partial<Storage>) {
+    Object.assign(this, data);
+  }
+
+  public static async createStorage(
+    storageBody: StorageType & {
+      created_at?;
+      updated_at?;
+    },
+    ncMeta = Noco.ncMeta
+  ): Promise<Storage> {
+    const insertObj = {
+      fk_base_id: storageBody.fk_base_id,
+      fk_project_id: storageBody.fk_project_id,
+      source: storageBody.source,
+      meta: storageBody.meta,
+    };
+    const { id: storageId } = await ncMeta.metaInsert2(
+      null,
+      null,
+      MetaTable.STORAGES,
+      insertObj
+    );
+
+    await NocoCache.appendToList(
+      CacheScope.STORAGE,
+      [insertObj.fk_project_id],
+      `${CacheScope.STORAGE}:${storageId}`
+    );
+    return this.get(storageId, ncMeta);
+  }
+
+  public static async list(
+    { projectId }: { projectId: string },
+    ncMeta = Noco.ncMeta
+  ): Promise<Storage[]> {
+    if (!projectId) return null;
+    let storageList = await NocoCache.getList(CacheScope.STORAGE, [projectId]);
+    if (!storageList.length) {
+      storageList = await ncMeta.metaList2(null, null, MetaTable.STORAGES, {
+        condition: { fk_project_id: projectId },
+      });
+      await NocoCache.setList(CacheScope.STORAGE, [projectId], storageList);
+    }
+    return storageList.map((s) => new Storage(s));
+  }
+
+  public static async update(storageId, storage, ncMeta = Noco.ncMeta) {
+    // get existing cache
+    const key = `${CacheScope.STORAGE}:${storageId}`;
+    const updateObj = extractProps(storage, ['source', 'meta']);
+    let o = await NocoCache.get(key, CacheGetType.TYPE_OBJECT);
+    if (o) {
+      o = { ...o, ...updateObj };
+      // set cache
+      await NocoCache.set(key, o);
+    }
+    // set meta
+    await ncMeta.metaUpdate(
+      null,
+      null,
+      MetaTable.STORAGES,
+      updateObj,
+      storageId
+    );
+  }
+
+  public static async delete(storageId: string, ncMeta = Noco.ncMeta) {
+    await NocoCache.deepDel(
+      CacheScope.STORAGE,
+      `${CacheScope.STORAGE}:${storageId}`,
+      CacheDelDirection.CHILD_TO_PARENT
+    );
+    await ncMeta.metaDelete(null, null, MetaTable.STORAGES, storageId);
+  }
+
+  public static async get(storageId: any, ncMeta = Noco.ncMeta) {
+    let storageData =
+      storageId &&
+      (await NocoCache.get(
+        `${CacheScope.STORAGE}:${storageId}`,
+        CacheGetType.TYPE_OBJECT
+      ));
+    if (!storageData) {
+      storageData = await ncMeta.metaGet2(
+        null,
+        null,
+        MetaTable.STORAGES,
+        storageId
+      );
+      await NocoCache.set(`${CacheScope.STORAGE}:${storageId}`, storageData);
+    }
+    return storageData && new Storage(storageData);
+  }
+}
