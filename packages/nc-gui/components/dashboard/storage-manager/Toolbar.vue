32c25adca59e14106ad0cee4e9c5c3252212c091
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-08 17:10:12 +0800

    feat(nc-gui): add storage manager toolbar items

--- packages/nc-gui/components/dashboard/storage-manager/Toolbar.vue
+++ packages/nc-gui/components/dashboard/storage-manager/Toolbar.vue
@@ -2,11 +2,54 @@
 
 <template>
   <div class="my-4">
-    <!-- TODO: New Folder -->
-    <!-- TODO: Upload -->
-    <!-- TODO: Sort By -->
-    <!-- TODO: Refresh -->
-    <!-- TODO: Download (when selected) -->
-    <!-- TODO: Delete (when selected) -->
+    <!-- New Folder -->
+    <a-button v-e="['c:stoage:folder']" class="nc-new-folder-menu-btn nc-toolbar-btn">
+      <div class="flex items-center gap-1">
+        <MdiFolderOutline />
+        <span class="text-capitalize !text-sm font-weight-normal">{{ $t('activity.storageManager.newFolder') }}</span>
+      </div>
+    </a-button>
+
+    <!-- Upload File -->
+    <a-button v-e="['c:storage:upload']" class="nc-upload-folder-menu-btn nc-toolbar-btn">
+      <div class="flex items-center gap-1">
+        <MdiUploadOutline />
+        <span class="text-capitalize !text-sm font-weight-normal">{{ $t('activity.storageManager.uploadFile') }}</span>
+      </div>
+    </a-button>
+
+    <!-- Sort By -->
+    <a-button v-e="['c:storage:sortBy']" class="nc-sort-by-menu-btn nc-toolbar-btn">
+      <div class="flex items-center gap-1">
+        <MdiSort />
+        <span class="text-capitalize !text-sm font-weight-normal">{{ $t('activity.storageManager.sortBy') }}</span>
+        <MdiMenuDown class="text-grey" />
+      </div>
+    </a-button>
+
+    <!-- Refresh -->
+    <a-button v-e="['c:storage:refresh']" class="nc-refresh-menu-btn nc-toolbar-btn">
+      <div class="flex items-center gap-1">
+        <MdiRefresh />
+        <span class="text-capitalize !text-sm font-weight-normal">{{ $t('activity.storageManager.refresh') }}</span>
+        <MdiMenuDown class="text-grey" />
+      </div>
+    </a-button>
+
+    <!-- Download (when selected) -->
+    <a-button v-e="['c:storage:download']" class="nc-download-menu-btn nc-toolbar-btn">
+      <div class="flex items-center gap-1">
+        <MdiDownload />
+        <span class="text-capitalize !text-sm font-weight-normal">{{ $t('activity.storageManager.download') }}</span>
+      </div>
+    </a-button>
+
+    <!-- Delete (when selected) -->
+    <a-button v-e="['c:storage:delete']" class="nc-delete-menu-btn nc-toolbar-btn">
+      <div class="flex items-center gap-1">
+        <MdiDeleteOutline />
+        <span class="text-capitalize !text-sm font-weight-normal">{{ $t('activity.storageManager.delete') }}</span>
+      </div>
+    </a-button>
   </div>
 </template>
