52d503d9e4e15ca249a918d9885ec6d3a95e8e18
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-08 16:25:37 +0800

    wip(nc-gui): storage manager search

---
+++ packages/nc-gui/components/dashboard/storage-manager/Search.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const searchQuery = ref<string>()
+
+function search() {
+  // TODO
+}
+</script>
+
+<template>
+  <div class="flex flex-row border-1 rounded-sm">
+    <div ref="searchDropdown" class="flex items-center relative bg-gray-50 px-2 cursor-pointer border-r-1">
+      <MdiMagnify class="text-grey" />
+    </div>
+    <a-input
+      size="small"
+      v-model:value="searchQuery"
+      class="max-w-[200px]"
+      placeholder="Search"
+      :bordered="false"
+      @press-enter="search"
+    >
+    </a-input>
+  </div>
+</template>
