3b4c4c05fefe83ef258a205472db11f61d2f0a17
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-08 14:12:11 +0800

    wip(nc-gui): storage toolbar

---
+++ packages/nc-gui/components/dashboard/storage-manager/Toolbar.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="my-4">
+    <!-- TODO: New Folder -->
+    <!-- TODO: Upload -->
+    <!-- TODO: Sort By -->
+    <!-- TODO: Refresh -->
+    <!-- TODO: Download (when selected) -->
+    <!-- TODO: Delete (when selected) -->
+  </div>
+</template>
