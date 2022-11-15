5b0d54e9d8be724800b2dc465437656d2581ae1b
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-15 16:31:46 +0800

    feat(nc-gui): use importStepper from quick import store

--- packages/nc-gui/components/quick-import/Stepper.vue
+++ packages/nc-gui/components/quick-import/Stepper.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-const current = ref<number>(0)
+import { useQuickImportStoreOrThrow } from '#imports'
+
+const { importStepper } = useQuickImportStoreOrThrow()!
 </script>
 
 <template>
-  <a-steps v-model:current="current" type="navigation">
+  <a-steps v-model:current="importStepper" type="navigation">
     <a-step title="Upload Data" description="" />
     <a-step title="Review Data" description="" />
     <a-step title="Load To Database" description="" />
