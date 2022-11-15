880986c8b3dff7c70133985af39fae2a0df9d4e7
Wing-Kam Wong <wingkwong.code@gmail.com>
2022-11-15 13:59:37 +0800

    feat(nc-gui): add a basic quick import stepper

---
+++ packages/nc-gui/components/quick-import/Stepper.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const current = ref<number>(0)
+</script>
+
+<template>
+  <a-steps v-model:current="current" type="navigation">
+    <a-step title="Upload Data" description="" />
+    <a-step title="Review Data" description="" />
+    <a-step title="Load To Database" description="" />
+  </a-steps>
+</template>
